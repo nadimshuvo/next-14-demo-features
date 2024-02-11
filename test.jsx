3a375e1cc0c3cf996c4470f7a7f07d0408f508bd
nadimshuvo <nadimshuvo1997@gmail.com>
2024-02-11 10:56:57 +0600

    updated

--- test.jsx
+++ test.jsx
@@ -1,15 +1,58 @@
-function App() {
-  const quantities = [1, 2, 3];
-
-  return <ClockList quantities={quantities} />;
+function BoilingVerdict({ celsius = 0 }) {
+  if (celsius >= 100) {
+    return <p>The water would boil.</p>;
+  } else {
+    return <p>The water would not boil.</p>;
+  }
 }
 
-function ClockList({ quantities = [] }) {
+function TemperatureInput({ temperature, scale, onTemperatureChange }) {
+  const scaleNames = {
+    c: "Celsius",
+    f: "Fahrenheit",
+  };
+
   return (
-    <div>
-      {quantities.map((key) => {
-        <Clock key={key} />;
-      })}
-    </div>
+    <fieldset>
+      <legend>Enter temperature in {scaleNames[scale]}</legend>
+      <input
+        type="text"
+        value={temperature}
+        onChange={(e) => onTemperatureChange(e, scale)}
+      />
+    </fieldset>
   );
+}
+
+class Calculator extends React.Component() {
+  state = {
+    temperature: "",
+    scale: "c",
+  };
+
+  handleChange = (e, scale) => {
+    this.setState({
+      temperature: e.target.value,
+      scale,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleChange}
+        />
+
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleChange}
+        />
+        <BoilingVerdict celsius={parseFloat(celsius)} />
+      </div>
+    );
+  }
 }

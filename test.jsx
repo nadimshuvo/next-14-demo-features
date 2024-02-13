79c51cdc20d63e679367ff255b384920ea62e494
nadimshuvo <nadimshuvo1997@gmail.com>
2024-02-13 09:54:21 +0600

    updated

---
+++ test.jsx
@@ -0,0 +1,9 @@
+export class Emoji extends React.Component {
+  addEmoji = (text, emoji) => `${emoji} ${text} ${emoji}`;
+
+  render(override) {
+    let text = "I am the Emoji Component";
+
+    if (override) text = override;
+  }
+}
